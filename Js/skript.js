6113ab8869820662dd8a0a662a7476de7d24ff83
antoninaMuseDev <82577937+antoninaMuseDev@users.noreply.github.com>
2021-08-15 22:48:32 +0300

    это последнее

--- Js/skript.js
+++ Js/skript.js
@@ -1,24 +1,13 @@
-let slideIndex = 1;
-showSlades(slideIndex);
 
-function plusSlides(n) {
-    showSlades(slideIndex += n);
-}
-function currentSlide(n) {
-    showSlades(slideIndex = n);
-}
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("swiper-slider");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slideslength) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className.replace(" activ", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += "activ"
-}
+const mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+  });
+
+const mySwiper = document.querySelector('.swiper-container').swiper
+
+
+    
+  

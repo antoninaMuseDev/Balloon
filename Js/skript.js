03312d5ccf0943d4ac058ef4d74d0d256dd6516d
antoninaMuseDev <82577937+antoninaMuseDev@users.noreply.github.com>
2021-08-16 15:34:12 +0300

    16 августа15.30

--- Js/skript.js
+++ Js/skript.js
@@ -1,3 +1,17 @@
+function openPrice(evt, priceName) {
+    const i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("price-tabs__tabcontent");
+    for (i=0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("price-tabs__tablinks");
+    for (i=0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+document.getElementsById(priceName).style.display = "block";
+evt.currentTarget.className += "active";
+    }
+}
+
 new Swiper('.comment-slide', {
     navigation: {
         nextEl: '.swiper-button-next',
@@ -13,6 +27,7 @@ new Swiper('.comment-slide', {
     },
 
 });
+
 
 
 
